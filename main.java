1384e78e65ccff7b47a4b6793802f32f9613c1af
Andres ALzate <andresfelipeafp@hotmail.com>
2023-09-21 19:12:09 -0500

    frase corregida

--- main.java
+++ main.java
@@ -3,12 +3,7 @@ public class main {
 
     public static void main(String[] args){
 
-        System.out.println("Hola mundo");
+        System.out.println("a commit has been made");
 
-        int variable = 10 ;
-
-        int variable2 = 15;
-
-        System.out.println(variable2+variable);
     }
 }
